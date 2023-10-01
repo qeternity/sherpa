46bdc27b3157cd02941632730dd417eb0e4d1261
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-10-01 13:26:25 +0100

    .

--- sherpa/test_guidance.py
+++ sherpa/test_guidance.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append("/root/sherpa/exllamav2")
 sys.path.append("/root/sherpa/guidance")
 import guidance
-from transformers import AutoTokenizer
+from transformers import AutoConfig, AutoTokenizer
 from exllamav2_hf import Exllamav2HF
 
+config = AutoConfig.from_pretrained("/root/Nous-Hermes-Llama2-13b-GPTQ")
 tokenizer = AutoTokenizer.from_pretrained("/root/Nous-Hermes-Llama2-13b-GPTQ")
 model = Exllamav2HF.from_pretrained("/root/Nous-Hermes-Llama2-13b-GPTQ")
+model.config = config
 
 guidance.llm = guidance.llms.Transformers(model, tokenizer, device=0)
 
