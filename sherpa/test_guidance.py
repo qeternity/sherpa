b2badd57e5c032b18e489c1e9c1527fde23f799d
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-10-01 13:15:30 +0100

    .

---
+++ sherpa/test_guidance.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.path.append("/root/sherpa/exllamav2")
+sys.path.append("/root/sherpa/guidance")
+import guidance
+from transformers import AutoTokenizer
+from exllamav2_hf import Exllamav2HF
+
+tokenizer = AutoTokenizer.from_pretrained("/root/Nous-Hermes-Llama2-13b-GPTQ")
+model = Exllamav2HF.from_pretrained("/root/Nous-Hermes-Llama2-13b-GPTQ")
+
+guidance.llm = guidance.llms.Transformers(model, tokenizer, device=0)
+
+guidance(
+    """The best thing about the beach is {{~gen 'best' temperature=0.7 max_tokens=7}}"""
+)()
