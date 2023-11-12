9075a2e70c60305b75328fa8cd030a5dba7727ad
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-11-12 11:05:57 +0000

    .

--- sherpa/api.py
+++ sherpa/api.py
@@ -69,11 +69,7 @@ async def stream_data(req: GenerateRequest):
         resp.pop("@raw_prefix", None)
         resp.pop("logging", None)
         resp.pop("accelerate", None)
-        return JSONResponse(
-            [
-                json.dumps(resp, indent=4),
-            ]
-        )
+        return JSONResponse(resp)
     finally:
         semaphore.release()
 
