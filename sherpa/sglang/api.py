31ba7a52a2515a9bd5d6ee90d5450aef1dade758
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2024-02-07 17:26:21 +0000

    sglang

---
+++ sherpa/sglang/api.py
@@ -0,0 +1,106 @@
+import argparse
+import asyncio
+from contextlib import asynccontextmanager
+import json
+import sys
+import time
+from typing import Any, Dict, List, Optional, Union
+
+from prompt import Prompt
+from prompt_async import Prompt as AsyncPrompt
+
+sys.path.append("/root/sherpa/exllamav2")
+
+import elasticapm
+import torch
+import uvicorn
+from elasticapm.contrib.starlette import ElasticAPM, make_apm_client
+from exllamav2 import (
+    ExLlamaV2,
+    ExLlamaV2Cache,
+    ExLlamaV2Config,
+    ExLlamaV2Tokenizer,
+)
+from exllamav2.generator import ExLlamaV2StreamingGenerator, ExLlamaV2Sampler, ExLlamaV2BatchedGeneratorAsync, ExLlamaV2BatchedModelAsync
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+torch.inference_mode()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, required=True, help="model path")
+parser.add_argument("--port", type=int, required=True, help="api port")
+parser.add_argument("--batches", type=int, required=False, help="num batches")
+
+NUM_BATCHES = 1
+
+tokenizer = None
+model = None
+cache = None
+generator = None
+settings = ExLlamaV2Sampler.Settings()
+settings.temperature = 0
+settings.top_k = 1
+generator_queue = asyncio.Queue()
+
+
+class GenerateRequest(BaseModel):
+    prompt: str
+
+@asynccontextmanager
+async def lifespan(app):
+    await generator_queue.put(generator)
+    for i in range(NUM_BATCHES - 1):
+        await generator_queue.put(ExLlamaV2BatchedGeneratorAsync(model, cache.clone(), tokenizer))
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+# apm = make_apm_client(
+#     {
+#         "SERVER_URL": "https://apm.zuma.dev/",
+#         "SERVICE_NAME": "sherpa",
+#         "SECRET_TOKEN": "",
+#         "SPAN_COMPRESSION_EXACT_MATCH_MAX_DURATION": "0ms",
+#     }
+# )
+# app.add_middleware(ElasticAPM, client=apm)
+
+
+@app.get("/healthz")
+async def stream_data():
+    return JSONResponse({"status": "gucci"})
+
+
+@app.post("/generate")
+async def stream_data(req: GenerateRequest):
+    generator = await generator_queue.get()
+    try:
+        t0 = time.time()
+        context = await AsyncPrompt(tokenizer, generator, settings, req.prompt)()
+        t1 = time.time()
+        _sec = t1 - t0
+        print(f"Generated {context.token_count} tokens in {_sec}")
+        return JSONResponse(context.vars)
+    finally:
+        await generator_queue.put(generator)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.batches:
+        NUM_BATCHES = args.batches
+
+    model_path = args.model
+    config = ExLlamaV2Config()
+    config.model_dir = model_path
+    config.prepare()
+    tokenizer = ExLlamaV2Tokenizer(config)
+    model = ExLlamaV2BatchedModelAsync(config, NUM_BATCHES)
+    cache = ExLlamaV2Cache(model, lazy=True)
+    model.load_autosplit(cache)
+    generator = ExLlamaV2BatchedGeneratorAsync(model, cache, tokenizer)
+
+    uvicorn.run(app, host="0.0.0.0", port=args.port)
---
+++ sherpa/sglang/prompt_async.py
@@ -0,0 +1,204 @@
+import sys
+import re
+
+sys.path.append("/root/sherpa/exllamav2/")
+
+from typing import Any, Dict, Optional
+
+from sglang import (
+    function,
+    gen,
+    set_default_backend,
+    RuntimeEndpoint,
+)
+
+set_default_backend(RuntimeEndpoint("http://localhost:30000"))
+
+# returns groups for cmd, name and args
+op_matcher = re.compile(
+    r"""(?P<pre>[^\{]*){{\s*(?P<cmd>[a-z]+)\s*\"(?P<name>\w+)\"(?P<args>[^\}]*)}}(?P<post>[^\{]*)"""
+)
+args_matcher = re.compile(r"""\s*(?P<key>\w+)=\"(?P<value>[^"]+)\"""")
+
+
+class Context:
+    def __init__(self, prompt: str, tokenizer, generator, settings) -> None:
+        self.prompt = prompt
+        self.tokenizer = tokenizer
+        self.generator = generator
+        self.settings = settings
+
+        self.draft = ""
+        self.vars: Dict[str, Any] = dict()
+
+        self.token_count = 0
+
+
+class Op:
+    pass
+
+
+class NamedOp(Op):
+    NULL = "null"
+
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.name = name
+
+    def _return_null(self, context: Context) -> Context:
+        context.draft += self.NULL
+        context.vars[self.name] = None
+        return context
+
+
+class Echo(Op):
+    def __init__(self, value: str) -> None:
+        super().__init__()
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"""Echo(value="{self.value.encode("unicode_escape").decode()}")"""
+
+    async def run(self, context: Context) -> Context:
+        context.draft += self.value
+        return context
+
+
+class Generate(NamedOp):
+    DEFAULT_STOP_STRINGS = [
+        "<|im_end|>",
+        "</s>",
+    ]
+
+    def __init__(
+        self,
+        name: str,
+        max_tokens: Optional[str] = None,
+        regex: Optional[str] = None,
+        stop_regex: Optional[str] = None,
+        depends: Optional[str] = None,
+    ) -> None:
+        super().__init__(name)
+        self.max_tokens = int(max_tokens.strip()) if max_tokens else None
+        stop_list = [re.escape(s) for s in self.DEFAULT_STOP_STRINGS]
+        if stop_regex:
+            stop_list.append(stop_regex)
+        self.stop_regex = rf"({'|'.join(stop_list)})"
+        if regex:
+            assert stop_regex is not None
+        self.regex = rf"({regex}|null)" if regex else None
+        self.depends = depends
+
+    def __repr__(self) -> str:
+        return f"Generate(name={self.name}, max_tokens={self.max_tokens}, stop_regex={self.stop_regex})"
+
+    @function
+    def gen(self, s, context: Context):
+        s += context.draft.strip(" ")
+        s += gen("gen", temperature=0, regex=self.regex)
+    
+    async def run(self, s, context: Context) -> Context:
+        if self.depends and context.vars.get(self.depends) is None:
+            return self._return_null(context)
+        
+        cnt = 0
+        draft = ""
+        state = self.gen(context=context)
+        async for chunk in state.text_async_iter():
+            cnt += 1
+            draft += chunk
+            if self.stop_regex and re.search(self.stop_regex, draft):
+                break
+
+        if self.stop_regex and re.search(self.stop_regex, draft):
+            draft = re.split(self.stop_regex, draft, maxsplit=1)[0]
+
+        context.draft += draft
+        context.vars[self.name] = draft if draft != self.NULL else None
+        context.token_count += cnt
+        return context
+
+
+# class Select(NamedOp):
+#     def __init__(
+#         self,
+#         name: str,
+#         options: str,
+#         depends: Optional[str] = None,
+#     ) -> None:
+#         super().__init__(name)
+#         self.options = options.split(",")
+#         self.depends = depends
+
+#     def __repr__(self) -> str:
+#         return f"Select(name={self.name}, options={self.options})"
+
+#     async def run(self, context: Context) -> Context:
+#         if self.depends and context.vars.get(self.depends) is None:
+#             return self._return_null(context)
+
+#         settings = context.settings.clone()
+#         settings.filters = [ExLlamaV2SelectFilter(context.generator.model, context.tokenizer, self.options),]
+
+#         input_ids = context.tokenizer.encode(context.draft)
+#         await context.generator.begin_stream(input_ids, settings)
+#         # await context.generator.begin_stream(input_ids, settings, token_healing=True)
+
+#         cnt = 0
+#         draft = ""
+#         # context.generator.first_token = True # hack, not sure why needed for select with token healing
+#         while True:
+#             chunk, eos, _ = await context.generator.stream()
+#             cnt += 1
+#             draft += chunk
+#             if eos:
+#                 break
+
+#         context.draft += draft
+#         context.vars[self.name] = draft if draft != self.NULL else None
+#         context.token_count += cnt
+#         return context
+
+
+class Prompt:
+    # def __init__(self, generator: ExLlamaV2StreamingGenerator, prompt: str) -> None:
+    def __init__(self, tokenizer, generator, settings, prompt: str) -> None:
+        self.context = Context(prompt, tokenizer, generator, settings)
+        self.ops = list()
+
+    async def __call__(self) -> Context:
+        self._prepare()
+        await self._run()
+        return self.context
+
+    def _prepare(self) -> None:
+        for op_match in op_matcher.finditer(self.context.prompt):
+            pre = op_match.group("pre")
+            cmd = op_match.group("cmd")
+            name = op_match.group("name")
+            args = op_match.group("args")
+            post = op_match.group("post")
+
+            if pre:
+                self.ops.append(Echo(pre))
+
+            if cmd == "gen":
+                klass = Generate
+            elif cmd == "select":
+                # klass = Select
+                raise RuntimeError("Select not supported")
+            else:
+                raise ValueError(f"Unknown command: {cmd}")
+
+            args_dict = dict()
+            for args_match in args_matcher.finditer(args):
+                args_dict[args_match.group("key")] = args_match.group("value")
+
+            self.ops.append(klass(name, **args_dict))
+
+            if post:
+                self.ops.append(Echo(post))
+
+    async def _run(self) -> None:
+        for op in self.ops:
+            self.context = await op.run(self.context)
